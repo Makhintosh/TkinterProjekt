8dac994e31d0ea768dbffa110c050e5bc6e9177f
Igor Szatkowski <igor.szatkowski@gmail.com>
2023-05-11 13:55:01 +0200

    Projekt strony oraz kod

--- zadanie.py
+++ zadanie.py
@@ -9,10 +9,20 @@ firma_label.pack(pady=10)
 imie_entry = tk.Entry(okno, width=30)
 imie_entry.pack(pady=5)
 
-miasta = ["Warszawa", "Kraków", "Gdańsk", "Olsztyn", "Poznać", "Wrocław"]
+nazwisko_entry = tk.Entry(okno, width=30)
+nazwisko_entry.pack(pady=5)
+
+wiek_entry = tk.Entry(okno, width=10)
+wiek_entry.pack(pady=5)
+
+miasta = ["Warszawa", "Kraków", "Gdańsk", "Olsztyn", "Poznań", "Wrocław"]
 miasta_var = tk.StringVar(value=miasta[0])
 miasta_dropdown = tk.OptionMenu(okno, miasta_var, *miasta)
 miasta_dropdown.pack(pady=5)
 
-wiek_entry = tk.Entry(okno, width=10)
-wiek_entry.pack(pady=5)
+def wyslij_form():
+    imie = imie_entry.get()
+    nazwisko = nazwisko_entry.get()
+    wiek = wiek_entry.get()
+    miasta = miasta_var.get()
+
