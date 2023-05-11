4eea04155ede3745e75212bfe8e67d26af0016d2
Igor Szatkowski <igor.szatkowski@gmail.com>
2023-05-11 13:24:18 +0200

    Label firmy

---
+++ zadanie.py
@@ -0,0 +1,7 @@
+import tkinter as tk
+
+okno = tk.Tk()
+okno.title("Zakład pogrzebowy")
+
+firma_label = tk.Label(okno, text="Zakład pogrzebowy 'Żyć nie umierać'", font=("Arial", 16, "bold"))
+firma_label.pack(pady=10)
