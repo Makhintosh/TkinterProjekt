0098026d954f9a24ebbb2a5ecb7664b9b790a444
Igor Szatkowski <igor.szatkowski@gmail.com>
2023-05-18 13:58:29 +0200

    Poprawki

--- zadanie.py
+++ zadanie.py
@@ -1,9 +1,10 @@
 import tkinter as tk
+from PIL import Image, ImageTk
 
 def submit_form():
     first_name = first_name_entry.get()
     last_name = last_name_entry.get()
-    age = age_entry.get()
+    age = age_spin.get()
     city = city_dropdown.get()
     height = height_slider.get()
 
@@ -12,6 +13,15 @@ def submit_form():
         return
     
     result_label.config(text=f"Zarejestrowano {first_name} {last_name}, {age} lat, z {city}, wzrost: {height}cm")
+
+def reset_form():
+    first_name_entry.delete(0, tk.END)
+    last_name_entry.delete(0, tk.END)
+    age_spin.delete(0, tk.END)
+    age_spin.insert(13, 13)
+    city_dropdown.set(cities[0])
+    height_slider.set(100)
+    result_label.config(text="")
 
 okno = tk.Tk()
 okno.title("Zakład pogrzebowy")
@@ -33,8 +43,8 @@ last_name_entry.grid(row=1, column=1, padx=10, pady=5)
 
 age_label = tk.Label(form_frame, text="Wiek:")
 age_label.grid(row=2, column=0, padx=10, pady=5)
-age_entry = tk.Spinbox(form_frame, from_=13, to=120)
-age_entry.grid(row=2, column=1, padx=10, pady=5)
+age_spin = tk.Spinbox(form_frame, from_=13, to=120)
+age_spin.grid(row=2, column=1, padx=10, pady=5)
 
 city_label = tk.Label(form_frame, text="Miasta")
 city_label.grid(row=3, column=0, padx=10, pady=5)
@@ -50,6 +60,9 @@ height_slider.grid(row=4, column=1, padx=10, pady=5)
 
 submit_button = tk.Button(form_frame, text="Zatwierdź", command=submit_form)
 submit_button.grid(row=5, column=0, padx=10, pady=20)
+
+reset_button = tk.Button(form_frame, text="Reset", command=reset_form)
+reset_button.grid(row=5, column=1, padx=10, pady=20)
 
 result_label = tk.Label(form_frame, text="")
 result_label.grid(row=6, column=0, columnspan=2)
