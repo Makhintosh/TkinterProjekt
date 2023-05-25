f4d6c12c0dae4606113e2d8e10ef7fd6f8df8b69
Igor Szatkowski <igor.szatkowski@gmail.com>
2023-05-25 12:46:00 +0200

    Poprawy w kodzie + zdjęcie

--- zadanie.py
+++ zadanie.py
@@ -1,5 +1,5 @@
 import tkinter as tk
-from PIL import Image, ImageTk
+
 
 def submit_form():
     first_name = first_name_entry.get()
@@ -11,8 +11,10 @@ def submit_form():
     if not first_name or not last_name or not age or not city:
         result_label.config(text="Uzupełnij brakujące pola")
         return
-    
-    result_label.config(text=f"Zarejestrowano {first_name} {last_name}, {age} lat, z {city}, wzrost: {height}cm")
+
+    result_label.config(
+        text=f"Zarejestrowano {first_name} {last_name}, {age} lat, z {city}, wzrost: {height}cm")
+
 
 def reset_form():
     first_name_entry.delete(0, tk.END)
@@ -23,10 +25,13 @@ def reset_form():
     height_slider.set(100)
     result_label.config(text="")
 
+
 okno = tk.Tk()
 okno.title("Zakład pogrzebowy")
+okno.geometry("700x450")
 
-company_label = tk.Label(okno, text="Zakład pogrzebowy żyć nie umierać", font=("", 20, "bold"))
+company_label = tk.Label(
+    okno, text="Zakład pogrzebowy żyć nie umierać", font=("", 20, "bold"))
 company_label.pack(pady=20)
 
 form_frame = tk.Frame(okno)
@@ -48,7 +53,8 @@ age_spin.grid(row=2, column=1, padx=10, pady=5)
 
 city_label = tk.Label(form_frame, text="Miasta")
 city_label.grid(row=3, column=0, padx=10, pady=5)
-cities = ["Warszawa", "Kraków", "Poznań", "Olsztyn", "Gdańsk", "Szczytno", "Wrocław"]
+cities = ["Warszawa", "Kraków", "Poznań",
+          "Olsztyn", "Gdańsk", "Szczytno", "Wrocław"]
 city_dropdown = tk.StringVar(value=cities[0])
 city_dropdown_menu = tk.OptionMenu(form_frame, city_dropdown, *cities)
 city_dropdown_menu.grid(row=3, column=1, padx=10, pady=5)
@@ -67,6 +73,6 @@ reset_button.grid(row=5, column=1, padx=10, pady=20)
 result_label = tk.Label(form_frame, text="")
 result_label.grid(row=6, column=0, columnspan=2)
 
-form_frame.pack(pady=20)
+form_frame.grid(pady="20")
 
 okno.mainloop()
